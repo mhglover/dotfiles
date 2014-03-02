bac52df1a3f4dc66eae2f2c0b144d2c6fd8aaba2
Matthew Harris Glover <matthew@harrisglover.com>
2014-03-02 11:39:13 -0600

    added rmate and set it up as the default editor for ssh sessions

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -5,8 +5,12 @@ if [[ ! "$SSH_TTY" && "$OSTYPE" =~ ^darwin ]]; then
   export LESSEDIT='subl %f'
   alias q='subl'
 else
-  export EDITOR=$(type nano pico vi vim 2>/dev/null | sed 's/ .*$//;q')
-  alias q="$EDITOR -w -z"
+    #use "subl" for rmate rather than trying to run subl via the alias
+    alias subl='rmate'
+    export EDITOR=$(type rmate vim vi nano pico 2>/dev/null | sed 's/ .*$//;q')
+    alias q="$EDITOR"
+    
+    # alias q="$EDITOR -w -z"
 fi
 
 export VISUAL="$EDITOR"
