d9bf8b7fba89c2e095c8ac6c2e3ba81b2578306e
Matthew Harris Glover <matthew@harrisglover.com>
2014-03-02 11:40:05 -0600

    fixed a dumb broken url

--- source/50_ssh.sh
+++ source/50_ssh.sh
@@ -2,7 +2,7 @@ function ssh-init() {
     #copy id files for passwordless login
     ssh-copy-id $1
     #run the abbreviated dotfiles command
-    ssh $1 "bash -c \"$(curl -fsSL https://raw.github.com/mhglover/dotfiles-bare/master/bin/dotfiles)\""
+    ssh $1 "bash -c \"$(curl -fsSL https://raw.github.com/mhglover/dotfiles/master/bin/dotfiles-bare)\""
     #connect
     ssh $1
 }
