4d2b9217468a0faa3026260ec485d8d1128eaa14
Matthew Harris Glover <EMAIL>
2014-03-03 09:04:22 -0600

    added alert alias

--- source/50_ubuntu.sh
+++ source/50_ubuntu.sh
@@ -10,6 +10,10 @@ alias search="apt-cache search"
 # Make 'less' more.
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
 
+# Add an "alert" alias for long running commands.  Use like so:
+#   sleep 10; alert
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
 # # Switch between already-downloaded node versions.
 # function node_ver() {
 #   (
