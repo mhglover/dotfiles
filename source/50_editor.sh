37b7b9c45cab867c24fbf979c7b097264f5dc58b
Matthew Harris Glover <EMAIL>
2014-03-03 09:01:01 -0600

    added a function to make it easier to pipe to subl

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -5,10 +5,32 @@ if [[ ! "$SSH_TTY" && "$OSTYPE" =~ ^darwin ]]; then
   export LESSEDIT='subl %f'
   alias q='subl'
 elif [[ ! "$SSH_TTY" && "$(cat /etc/issue 2> /dev/null)" =~ Ubuntu ]]; then
+  # Enable the use of the `subl` command
+  subl () {
+      subl_path='/usr/bin/subl'
+      if test -t 0
+      then
+          "$subl_path" $*
+      else
+          timestamp=`date +%s`
+          filename=$1
+          shift
+          if [ -z $filename ]; then filename=".temp$timestamp" ; fi
+          touch "$filename"
+          while read data
+          do
+              echo "$data" >> "$filename"
+          done
+          "$subl_path" "$filename" $*
+          sleep 1
+          rm "$filename"
+      fi
+  }
+
   export EDITOR='/usr/bin/subl -w'
   export LESSEDIT='/usr/bin/subl %f'
   alias q='/usr/bin/subl'
-  alias subl='/usr/bin/subl'
+
 else
     #use "subl" for rmate rather than trying to run subl via the alias
     alias subl='rmate'
