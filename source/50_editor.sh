19cd33f693e4c802169d221a87d816864c921c5a
Matthew Harris Glover <EMAIL>
2014-03-03 08:10:49 -0600

    fixes for sublime text on ubuntu

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -4,12 +4,16 @@ if [[ ! "$SSH_TTY" && "$OSTYPE" =~ ^darwin ]]; then
   export EDITOR='subl -w'
   export LESSEDIT='subl %f'
   alias q='subl'
+elif [[ ! "$SSH_TTY" && "$(cat /etc/issue 2> /dev/null)" =~ Ubuntu ]]; then
+  export EDITOR='/usr/bin/subl -w'
+  export LESSEDIT='/usr/bin/subl %f'
+  alias q='/usr/bin/subl'
+  alias subl='/usr/bin/subl'
 else
     #use "subl" for rmate rather than trying to run subl via the alias
     alias subl='rmate'
     export EDITOR=$(type rmate vim vi nano pico 2>/dev/null | sed 's/ .*$//;q')
     alias q="$EDITOR"
-    
     # alias q="$EDITOR -w -z"
 fi
 
