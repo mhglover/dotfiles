6e70e36d6a64207571c346a3c559ee6a720ad4ba
Matthew Harris Glover <EMAIL>
2014-03-04 16:43:38 -0600

    just a bit of commenting

--- conf/firsttime_reminder.sh
+++ conf/firsttime_reminder.sh
@@ -1,5 +1,5 @@
 # This file is sourced at the end of a first-time dotfiles install.
-shopt -s expand_aliases
+shopt -s expand_aliases # Aliases are not expanded when the shell is not interactive, unless this is set
 source ~/.bashrc
 
 # I'm forgetful. Just look at this repo's commits to see how many times I
