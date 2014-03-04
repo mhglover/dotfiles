8eaf0f06ac172456de91b3d4bce0af10bc002f7c
Matthew Harris Glover <EMAIL>
2014-03-04 16:44:00 -0600

    ditched my alias for deploying - moved it to 50_file.sh cleaned up my ssh-fuse remote mount script options for ubuntu

--- source/50_ssh.sh
+++ source/50_ssh.sh
@@ -1,12 +1,3 @@
-function ssh-init() {
-    #copy id files for passwordless login
-    ssh-copy-id "$1"
-    #run the abbreviated dotfiles command
-    ssh "$1" "bash -c \"$(curl -fsSL https://raw.github.com/mhglover/dotfiles/master/bin/dotfiles-bare)\""
-    #connect
-    ssh "$1"
-}
-
 #enable ssh autocompletion
 complete -W "$(echo $(grep '^ssh ' ~/.bash_history | sort -u | sed 's/^ssh //'))" ssh
 
@@ -26,7 +17,7 @@ rmount() {
     fi
     if [[ $(grep -i "host ${host%%:*}" ~/.ssh/config) != '' ]]; then
         mkdir -p ~/mounts/$mname > /dev/null
-        sshfs $host$folder ~/mounts/$mname -oauto_cache,reconnect && echo "mounted ~/mounts/$mname"
+        sshfs $host$folder ~/mounts/$mname -oauto_cache,reconnect,idmap=user,workaround=rename && echo "mounted ~/mounts/$mname"
     else
         echo "No entry found for ${host%%:*}"
         return 1
