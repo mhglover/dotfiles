231e77a573e10295a4701aa6619802488ea628b5
Matthew Harris Glover <EMAIL>
2014-05-30 07:55:46 -0500

    adding a script to quickly create and save local aliases

---
+++ source/60_aliases.sh
@@ -0,0 +1,24 @@
+# alias last and save
+# use `als c NAME` to chop off the last argument (for filenames/patterns)
+als() {
+    local aliasfile chop x
+    [[ $# == 0 ]] && echo "Name your alias" && return
+    if [[ $1 == "c" ]]; then
+        chop=true
+        shift
+    fi
+    aliasfile=~/.dotfiles/source/60_aliases.sh
+    touch $aliasfile
+    if [[ `cat "$aliasfile" |grep "alias ${1// /}="` != "" ]]; then
+        echo "Alias ${1// /} already exists"
+    else
+        x=`fc -l -2 | sed -e '$!{h;d;}' -e x | sed -e 's/.\{7\}//'`
+        if [[ $chop == true ]]; then
+            echo "Chopping..."
+            x=$(echo $x | rev | cut -d " " -f2- | rev)
+        fi
+        echo -e "\nalias ${1// /}=\"`echo $x|sed -e 's/ *$//'|sed -e 's/\"/\\\\"/g'`\"" >> $aliasfile && source $aliasfile
+        alias $1
+    fi
+}
+
