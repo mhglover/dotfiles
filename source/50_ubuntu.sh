ae5048cce11ab776747ce1143f3c51c1f304afc5
Matthew Harris Glover <mglover@cspire.com>
2015-02-06 19:57:46 -0600

    removed some junk I don't use

--- source/50_ubuntu.sh
+++ source/50_ubuntu.sh
@@ -10,28 +10,3 @@ alias search="apt-cache search"
 # Make 'less' more.
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
 
-# Add an "alert" alias for long running commands.  Use like so:
-#   sleep 10; alert
-alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
-
-# # Switch between already-downloaded node versions.
-# function node_ver() {
-#   (
-#     ver="${1#v}"
-#     nodes=()
-#     if [[ ! -e "/usr/local/src/node-v$ver" ]]; then
-#       shopt -s extglob
-#       shopt -s nullglob
-#       cd "/usr/local/src"
-#       eval 'for n in node-v*+([0-9]).+([0-9]).+([0-9]); do nodes=("${nodes[@]}" "${n#node-}"); done'
-#       [[ "$1" ]] && echo "Node.js version \"$1\" not found."
-#       echo "Valid versions are: ${nodes[*]}"
-#       [[ "$(type -P node)" ]] && echo "Current version is: $(node --version)"
-#       exit 1
-#     fi
-#     cd "/usr/local/src/node-v$ver"
-#     sudo make install >/dev/null 2>&1 &&
-#     echo "Node.js $(node --version) installed." ||
-#     echo "Error, $(node --version) installed."
-#   )
-# }
