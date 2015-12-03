5b3742b9b55d8ad1b4b0eec9ea20d70c710175be
Matthew Harris Glover <mglover@cspire.com>
2015-12-03 15:40:34 -0600

    changed how I handle python virtual environment stuff

--- source/10_misc.sh
+++ source/10_misc.sh
@@ -1,9 +1,10 @@
-export PIP_REQUIRE_VIRTUALENV=true
-export VIRTUAL_ENV_DISABLE_PROMPT=1
+if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
+# export PIP_REQUIRE_VIRTUALENV=true
+# export VIRTUAL_ENV_DISABLE_PROMPT=1
 
-gpip(){
-   PIP_REQUIRE_VIRTUALENV="" pip "$@"
-}
+# gpip(){
+#    PIP_REQUIRE_VIRTUALENV="" pip "$@"
+# }
 
 
 
