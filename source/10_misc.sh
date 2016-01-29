da93ad78144f35d98507493453fc50e117a03bd9
Matthew Harris Glover <mglover@cspire.com>
2016-01-29 12:57:17 -0600

    added some virtualenv integration

--- source/10_misc.sh
+++ source/10_misc.sh
@@ -1,11 +1,15 @@
 if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
-# export PIP_REQUIRE_VIRTUALENV=true
-# export VIRTUAL_ENV_DISABLE_PROMPT=1
+export PIP_REQUIRE_VIRTUALENV=true
+export VIRTUAL_ENV_DISABLE_PROMPT=1
 
-# gpip(){
-#    PIP_REQUIRE_VIRTUALENV="" pip "$@"
-# }
+gpip(){
+   PIP_REQUIRE_VIRTUALENV="" pip "$@"
+}
 
 
+export WORKON_HOME=$HOME/envs
+export PIP_VIRTUALENV_BASE=$WORKON_HOME
 
+export PROJECT_HOME=$HOME/devel
 source /usr/local/bin/virtualenvwrapper.sh
+alias workoff='deactivate'
