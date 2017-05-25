b5517a878ba1156fd7c57caf9447bb929da87600
Matthew Harris Glover <EMAIL>
2017-05-25 12:25:26 -0400

    pip, ruby, and java updates

--- source/10_misc.sh
+++ source/10_misc.sh
@@ -1,15 +1,24 @@
-# if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
-# export PIP_REQUIRE_VIRTUALENV=true
-# export VIRTUAL_ENV_DISABLE_PROMPT=1
+if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
+export PIP_REQUIRE_VIRTUALENV=true
+export VIRTUAL_ENV_DISABLE_PROMPT=1
 
-# gpip(){
-#    PIP_REQUIRE_VIRTUALENV="" pip "$@"
-# }
+gpip(){
+   PIP_REQUIRE_VIRTUALENV="" pip "$@"
+}
 
 
-# export WORKON_HOME=$HOME/envs
-# export PIP_VIRTUALENV_BASE=$WORKON_HOME
+export WORKON_HOME=$HOME/.virtualenvs
+export PIP_VIRTUALENV_BASE=$WORKON_HOME
 
-# export PROJECT_HOME=$HOME/devel
-# source /usr/local/bin/virtualenvwrapper.sh
-# alias workoff='deactivate'
+export PROJECT_HOME=$HOME/devel
+source /usr/local/bin/virtualenvwrapper.sh
+alias workoff='deactivate'
+
+
+# auto-execute the contents of an .env file in a directory when you cd into it
+source $(brew --prefix autoenv)/activate.sh
+
+# add rubyenv
+eval "$(rbenv init -)"
+
+export JAVA_HOME=$(/usr/libexec/java_home)
