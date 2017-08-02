565257d5b5f682846355e9a7a29178ddfe907076
Matthew Harris Glover <mglover@carnegielearning.com>
2017-08-02 12:26:56 -0400

    don't edit yourself

--- source/60_aliases.sh
+++ source/60_aliases.sh
@@ -2,13 +2,13 @@
 # use `als c NAME` to chop off the last argument (for filenames/patterns)
 # from http://brettterpstra.com/2013/08/30/easily-save-that-wicked-awesome-shell-command/
 als() {
-    aliasfile=~/.dotfiles/source/60_aliases.sh
-    #local aliasfile chop x
+    local aliasfile chop x
+    aliasfile=~/.dotfiles/source/70_als_aliases.sh
     if [[ $# == 0 ]] ; then
         echo "Usage:"
         echo "als <aliasname>   (to save the last complete command as a new alias)"
         echo "als c <aliasname> (to chop off the final argument in the saved command)"
-        echo "edit $aliasfile to remove aliases" 
+        echo "edit $aliasfile to remove aliases"
         return
     fi
     if [[ $1 == "c" ]]; then
@@ -19,7 +19,8 @@ als() {
     if [[ `cat "$aliasfile" |grep "alias ${1// /}="` != "" ]]; then
         echo "Alias ${1// /} already exists"
     else
-        x=`fc -l -2 | sed -e '$!{h;d;}' -e x | sed -e 's/.\{7\}//'`
+        # slightly modified because I add datestamps to my bash history
+        x=`history 2 | sed -e '$!{h;d;}' -e x | sed -e 's/.\{7\}//' | cut -f 3- -d" "`
         if [[ $chop == true ]]; then
             echo "Chopping..."
             x=$(echo $x | rev | cut -d " " -f2- | rev)
