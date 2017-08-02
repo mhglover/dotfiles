33fd67cd14f8b7ef6ababcd49937146109d58949
Matthew Harris Glover <mglover@carnegielearning.com>
2017-08-02 12:24:59 -0400

    convenience aliases

--- source/50_ssh.sh
+++ source/50_ssh.sh
@@ -3,3 +3,7 @@
 if [[ -e ~/.ssh/known_hosts ]]; then
    complete -o default -W "$(cat ~/.ssh/known_hosts | sed 's/[, ].*//' | sort | uniq )" ssh scp sftp
 fi
+
+alias ssh-no-key="ssh -o PreferredAuthentications=password -o PubkeyAuthentication=no"
+alias sshpassonly="ssh -o PreferredAuthentications=password -o PubkeyAuthentication=no"
+alias bamboossh="ssh -i ~/.ssh/elasticbamboo.pk"
