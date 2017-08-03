5db687da0c8ce52fb05072d9b3b44e1bd54d94ee
Matthew Harris Glover <mglover@carnegielearning.com>
2017-08-03 10:57:54 -0400

    add readability

--- source/90_docker.sh
+++ source/90_docker.sh
@@ -8,4 +8,13 @@ function docker-login-prod () {
   $(aws --profile prod ecr get-login --no-include-email)
 }
 
-alias dockerclean="docker container prune -f ; docker volume prune -f ; docker images | grep none.*none | awk '{print $3}' | while read image; do docker rmi $image ;done"
+function dockerclean () {
+  docker container prune -f
+  docker volume prune -f
+  docker images \
+    | grep "none.*none" \
+    | awk '{print $3}' \
+    | while read image; do
+        docker rmi $image
+      done
+}
